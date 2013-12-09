4e36fb4d0463b02b09caadae9456f6fa0672e61f
cumessage <cumessage@gmail.com>
2013-12-09 18:33:00 +0800

    add drawing

--- clockgame-android/src/main/java/com/prosper/clockgame/frontend/ui/FootFallDetector.java
+++ clockgame-android/src/main/java/com/prosper/clockgame/frontend/ui/FootFallDetector.java
@@ -1,5 +1,9 @@
 package com.prosper.clockgame.frontend.ui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -8,6 +12,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.util.Log;
 
 public class FootFallDetector implements SensorEventListener {
@@ -18,15 +23,14 @@ public class FootFallDetector implements SensorEventListener {
 	private static final float FC_FOOT_FALL_DETECTION = 3.5F;
 	private static final long SECOND_TO_NANOSECOND = (long) 1e9;
 	private static final int ACCELERATION_VALUE_KEEP_SECONDS = 10;
-	
-	
-	private float geomagnetic[] = new float[3];
-	private float accelerometer[] = new float[3];
 
+
+	private float accelerometer[] = new float[3];
+	private float gyroscope[] = new float[3];
+	
 	private final Context context;
 	private SensorManager sensorManager;
 	private boolean active = false;
-	private final LinkedList<Acceleration> values = new LinkedList<Acceleration>();
 
 	public FootFallDetector(Context context) {
 		this.context = context;
@@ -35,14 +39,17 @@ public class FootFallDetector implements SensorEventListener {
 	public void start() {
 		sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
 		Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Sensor gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 		Sensor linearAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 		Sensor gravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 		Sensor magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-		sensorManager.registerListener(this, linearAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
-		sensorManager.registerListener(this, gravity, SensorManager.SENSOR_DELAY_FASTEST);
-		sensorManager.registerListener(this, magnetic, SensorManager.SENSOR_DELAY_FASTEST);
+		sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+//		sensorManager.registerListener(this, linearAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+//		sensorManager.registerListener(this, gravity, SensorManager.SENSOR_DELAY_FASTEST);
+//		sensorManager.registerListener(this, magnetic, SensorManager.SENSOR_DELAY_FASTEST);
 		active = true;
+		
 	}
 
 	public void stop() {
@@ -52,54 +59,65 @@ public class FootFallDetector implements SensorEventListener {
 
 	public void onSensorChanged(SensorEvent event) {
 		if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER
-				&& event.sensor.getType() != Sensor.TYPE_MAGNETIC_FIELD) {
-//				&& event.sensor.getType() != Sensor.TYPE_LINEAR_ACCELEROMETER
-//				&& event.sensor.getType() != Sensor.TYPE_GRAVITY) {
+				&& event.sensor.getType() != Sensor.TYPE_GYROSCOPE) {
+			//				&& event.sensor.getType() != Sensor.TYPE_LINEAR_ACCELEROMETER
+			//				&& event.sensor.getType() != Sensor.TYPE_GRAVITY) {
 			return;
 		}
-		
-		if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
-			accelerometer[0] = event.values[0];
-			accelerometer[1] = event.values[1];
-			accelerometer[2] = event.values[2];
+
+		String fileName = "";
+		String log = "";
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			fileName = "accelerometer.log";
+			log = Long.toString(event.timestamp) + "\t" 
+					+ Float.toString(event.values[0]) + "\t" + Float.toString(event.values[1]) + "\t" 
+					+ Float.toString(event.values[2]) + "\t" + Float.toString(getSum(event.values[0], event.values[1], event.values[2])) + "\n"; 
 		}
-		
-		if (event.sensor.getType() != Sensor.TYPE_MAGNETIC_FIELD) {
-			geomagnetic[0] = event.values[0];
-			geomagnetic[1] = event.values[1];
-			geomagnetic[2] = event.values[2];
+
+		if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+			fileName = "gyroscope.log";
+			log = Long.toString(event.timestamp) + "\t" 
+					+ Float.toString(event.values[0]) + "\t" + Float.toString(event.values[1]) + "\t" 
+					+ Float.toString(event.values[2]) + "\n"; 
 		}
-		
-		float[] rotationMatrix = new float[9];
-		float[] rotation = new float[3];
-		SensorManager.getRotationMatrix(rotationMatrix, null, accelerometer, geomagnetic);
-		SensorManager.getOrientation(rotationMatrix, rotation);  
-		
-		rotation[0] = (float) Math.toDegrees(rotation[0]);  
-        rotation[1] = (float) Math.toDegrees(rotation[1]);  
-        rotation[2] = (float) Math.toDegrees(rotation[2]);  
-		
+
 		//		Log.d(LOG_TAG, Arrays.toString(event.values) + " | " + total);
-		Log.d(LOG_TAG, Integer.toString(event.sensor.getType()) + " : " + Arrays.toString(accelerometer));
-		Log.d(LOG_TAG, Integer.toString(event.sensor.getType()) + " : " + Arrays.toString(rotation));
+		//		Log.d(LOG_TAG, Integer.toString(event.sensor.getType()) + " : " + Arrays.toString(accelerometer));
 
-//		Acceleration acceleration = new Acceleration();
-//		acceleration.timestamp = event.timestamp;
+		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+			File sdCardDir = Environment.getExternalStorageDirectory();
+			File saveFile = new File(sdCardDir, fileName);
+			FileOutputStream outStream;
+			try {
+				outStream = new FileOutputStream(saveFile, true);
+				outStream.write(log.getBytes());
+				outStream.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			throw new RuntimeException("sd is not mounted");
+		}
 
-//		Acceleration prevValue = values.isEmpty() ? null : values.getFirst();
-//		if (prevValue == null) {
-//			for (int i = 0; i < 3; i++) {
-//				acceleration.averagedValues[i] = event.values[i];
-//				acceleration.lowPassFilteredValues[i] = event.values[i];
-//			}
-//		} else {
-//			lowPassFilter(acceleration.averagedValues, event.values, event.timestamp, prevValue.averagedValues,
-//					prevValue.timestamp, FC_EARTH_GRAVITY_DETECTION);
-//			lowPassFilter(acceleration.lowPassFilteredValues, event.values, event.timestamp,
-//					prevValue.lowPassFilteredValues, prevValue.timestamp, FC_FOOT_FALL_DETECTION);
-//		}
-//		values.addFirst(acceleration);
-//		removeValuesOlderThan(event.timestamp - ACCELERATION_VALUE_KEEP_SECONDS * SECOND_TO_NANOSECOND);
+		//		Acceleration acceleration = new Acceleration();
+		//		acceleration.timestamp = event.timestamp;
+
+		//		Acceleration prevValue = values.isEmpty() ? null : values.getFirst();
+		//		if (prevValue == null) {
+		//			for (int i = 0; i < 3; i++) {
+		//				acceleration.averagedValues[i] = event.values[i];
+		//				acceleration.lowPassFilteredValues[i] = event.values[i];
+		//			}
+		//		} else {
+		//			lowPassFilter(acceleration.averagedValues, event.values, event.timestamp, prevValue.averagedValues,
+		//					prevValue.timestamp, FC_EARTH_GRAVITY_DETECTION);
+		//			lowPassFilter(acceleration.lowPassFilteredValues, event.values, event.timestamp,
+		//					prevValue.lowPassFilteredValues, prevValue.timestamp, FC_FOOT_FALL_DETECTION);
+		//		}
+		//		values.addFirst(acceleration);
+		//		removeValuesOlderThan(event.timestamp - ACCELERATION_VALUE_KEEP_SECONDS * SECOND_TO_NANOSECOND);
 	}
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -110,6 +128,10 @@ public class FootFallDetector implements SensorEventListener {
 	public int getCurrentCadence() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public float getSum(float x, float y, float z) {
+		return x*x + y*y + z*z;
 	}
 
 	private void lowPassFilter(float[] result, float[] currentValue, long currentTime, float[] prevValue,
@@ -124,31 +146,15 @@ public class FootFallDetector implements SensorEventListener {
 		}
 	}
 
-	private void removeValuesOlderThan(long timestamp) {
-		while (!values.isEmpty()) {
-			if (values.getLast().timestamp < timestamp) {
-				values.removeLast();
-			} else {
-				return;
-			}
-		}
-	}
-
-	private class Acceleration {
-		public long timestamp;
-		public float[] lowPassFilteredValues = new float[3];
-		public float[] averagedValues = new float[3];
-
-		@Override
-		public String toString() {
-			return String.format("Time,average,filtered,:,%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", timestamp,
-					averagedValues[0], averagedValues[1], averagedValues[2], lowPassFilteredValues[0],
-					lowPassFilteredValues[1], lowPassFilteredValues[2]);
-		}
-	}
-
-
-
+	//	private void removeValuesOlderThan(long timestamp) {
+	//		while (!values.isEmpty()) {
+	//			if (values.getLast().timestamp < timestamp) {
+	//				values.removeLast();
+	//			} else {
+	//				return;
+	//			}
+	//		}
+	//	}
 
 
 }
